e463ba45cb8972f18dfa4ad6bf62c03a9b0018f8
Pieter Feenstra <feenstra48@hotmail.com>
2017-12-11 23:58:19 +0100

    4 challenges
    Reverse a string
    Factorialize
    Palindrome
    Longest word in string

---
+++ ruby_challanges.rb
@@ -0,0 +1,42 @@
+# reverse a String single line comment
+puts "Pieter Feenstra".reverse
+
+=begin
+Factorialize a Number
+=end
+
+def Factorialize(number)
+  result = 1
+  for i in 1..number do
+    result *= i
+  end
+  return result
+end
+
+puts Factorialize(5)
+
+#palindrome
+def palindrome(str)
+  str.gsub!(/\W/ , "")
+  return str.reverse == str ? true : false
+end
+
+ puts palindrome("race,..////@@ car")
+
+
+# Find the Longest Word in a String
+def longest_string(str)
+    str = str.split(" ")
+   longest = 0
+   for st in str do
+      if st.length > longest
+        longest = st.length
+     end
+   end
+
+   return longest
+
+
+end
+
+puts longest_string("What if we try a super-long word such as otorhinolaryngology")
